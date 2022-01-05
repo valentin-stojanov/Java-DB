736742175e4c450d6ae2bab6aef46d5551597ed2
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-05 16:37:21 +0200

    MySQL Lab - Basic CRUD added 02. Select Employees with Filter.

--- MySQL/Lab/Basic-CRUD/crud.sql
+++ MySQL/Lab/Basic-CRUD/crud.sql
@@ -2,3 +2,8 @@
 SELECT id, first_name, last_name, job_title
 FROM employees
 ORDER BY id;
+
+#02. Select Employees with Filter
+SELECT id, concat(`first_name`, ' ', `last_name`) AS full_name, job_title ,salary
+FROM employees
+WHERE salary>1000.00;
