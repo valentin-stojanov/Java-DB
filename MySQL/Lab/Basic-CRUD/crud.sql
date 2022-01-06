e5130240f2df48c81963950eb0d4e330e883641d
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-06 12:00:31 +0200

    MySQL Lab - Basic CRUD added 05. Select Employees by Multiple Filters

--- MySQL/Lab/Basic-CRUD/crud.sql
+++ MySQL/Lab/Basic-CRUD/crud.sql
@@ -7,3 +7,9 @@ ORDER BY id;
 SELECT id, concat(`first_name`, ' ', `last_name`) AS full_name, job_title ,salary
 FROM employees
 WHERE salary>1000.00;
+
+#05. Select Employees by Multiple Filters
+SELECT *
+FROM `employees`
+WHERE department_id=4 AND salary >=1000
+ORDER BY id;
