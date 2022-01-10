6b83d06d6f321eb8f916a1d7f3b915f40d31c4c3
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-10 16:29:34 +0200

    MySQL Exercise - Basic CRUD added basic-CRUD.sql - ex. 2. Find All Information About Departments, 3.Find salary of Each Employee and 4. Find Full Name of Each Employee.

--- MySQL/Exercises/Basic-CRUD/basic-CRUD.sql
+++ MySQL/Exercises/Basic-CRUD/basic-CRUD.sql
@@ -1,7 +1,22 @@
 USE `soft_uni`;
 
-#1. Find All Information About Departments 
-SELECT * FROM `departments`
+# 1. Find All Information About Departments 
+SELECT *
+FROM `departments`
 ORDER BY `department_id`;
 
-#2. Find all Department Names
+# 2. Find all Department Names
+SELECT `name`
+FROM `departments`
+ORDER BY `department_id`;
+
+# 3. Find salary of Each Employee
+SELECT `first_name`, `last_name`, `salary`
+FROM `employees`;
+
+# 4. Find Full Name of Each Employee
+SELECT `first_name`, `middle_name`, `last_name`
+FROM `employees`
+ORDER BY `employee_id`;
+
+# 5
