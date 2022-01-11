11ad3a048d4791fcc1bdab3ee31e6a11641da48c
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-11 15:38:30 +0200

    MySQL Exercise - Basic CRUD added 6-10.

--- MySQL/Exercises/Basic-CRUD/basic-CRUD.sql
+++ MySQL/Exercises/Basic-CRUD/basic-CRUD.sql
@@ -22,3 +22,34 @@ ORDER BY `employee_id`;
 # 5. Find Email Address of Each Employee
 SELECT CONCAT(`first_name`, '.', `last_name`, '@softuni.bg') AS `full_email_address`
 FROM `employees`;
+
+# 6. Find All Different Employee's Salaries
+SELECT DISTINCT `salary`
+FROM `employees`;
+
+# 7. Find all Information About Employees
+SELECT * FROM `employees`
+WHERE `job_title` = 'Sales Representative'
+ORDER BY `employee_id`;
+
+# 8. Find Names of All Employees by salary in Range 
+SELECT `first_name`, `last_name`, `job_title`
+FROM `employees`
+#WHERE `salary` >= 20000 AND `salary` <= 30000;
+WHERE `salary` BETWEEN 20000 AND 30000;
+
+# 9.  Find Names of All Employees
+SELECT CONCAT_WS(' ', `first_name`, `middle_name`, `last_name`) AS `Full Name`
+FROM `employees`
+WHERE `salary` IN(25000, 14000, 12500, 23600);
+
+# 10. Find All Employees Without Manager
+SELECT `first_name`, `last_name`
+FROM `employees`
+WHERE `manager_id` IS NULL;
+
+# 11. Find All Employees with salary More Than 50000
+SELECT `first_name`, `last_name`, `salary`
+FROM `employees`
+WHERE `salary` > 50000
+ORDER BY `salary` DESC; 
