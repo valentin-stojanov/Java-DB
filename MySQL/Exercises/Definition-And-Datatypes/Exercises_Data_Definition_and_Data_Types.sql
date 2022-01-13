4e0e9954077c79ab1e0ff64f5b38ebcaf403a751
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-13 17:45:39 +0200

    MySQL Exercise Definition-And-Datatypes - added 13.Basic Insert

--- MySQL/Exercises/Definition-And-Datatypes/Exercises_Data_Definition_and_Data_Types.sql
+++ MySQL/Exercises/Definition-And-Datatypes/Exercises_Data_Definition_and_Data_Types.sql
@@ -101,3 +101,69 @@ DROP PRIMARY KEY,
 ADD PRIMARY KEY `pk_users` (`id`),
 ADD CONSTRAINT `uq_username`
 UNIQUE (`username`);
+
+# 11.Movies Database
+
+# 12.Car Rental Database
+
+# 13.Basic Insert
+CREATE DATABASE `soft_uni`;
+
+USE `soft_uni`;
+
+CREATE TABLE `towns`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE `addresses`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `address_text` VARCHAR(100) NOT NULL,
+    `town_id` INT NOT NULL,
+    CONSTRAINT `fk_addresses_towns`
+    FOREIGN KEY `addresses`(`town_id`) REFERENCES `towns`(`id`)
+);
+
+CREATE TABLE `departments`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20)
+);
+
+CREATE TABLE `employees`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `first_name` VARCHAR(30) NOT NULL,
+    `middle_name` VARCHAR(30) NOT NULL,
+    `lkast_name` VARCHAR(30) NOT NULL,
+    `job_title` VARCHAR(20),
+    `salary` DECIMAL,
+	`department_id` INT,
+    `hire_date` DATE,
+    `address_id` INT,
+    CONSTRAINT `fk_employees_departments`
+    FOREIGN KEY `employees`(`department_id`) REFERENCES `departments`(`id`),
+    CONSTRAINT `fk_employees_addresses`
+    FOREIGN KEY `employees`(`address_id`) REFERENCES `addresses`(`id`)
+);
+
+INSERT INTO `towns`
+VALUES
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna'),
+(4, 'Burgas');
+
+INSERT INTO `departments`
+VALUES
+(1, 'Engineering'),
+(2, 'Sales'),
+(3, 'Marketing'),
+(4, 'Software Development'),
+(5, 'Quality Assurance');
+
+INSERT INTO `employees` (`id`, `first_name`, `middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`)
+VALUES
+(1, 'Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),
+(2, 'Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),
+(3, 'Maria','Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
+(4, 'Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00),
+(5, 'Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
