5b7a777b61eb0a4cfdd39dcf6e87979952c6056c
valentin <valentin_stojanov@abv.bg>
2022-01-13 20:46:55 +0200

    MySQL Exercise Definition-And-Datatypes - added 15.Basic Select All Fields and Order Them

--- MySQL/Exercises/Definition-And-Datatypes/Exercises_Data_Definition_and_Data_Types.sql
+++ MySQL/Exercises/Definition-And-Datatypes/Exercises_Data_Definition_and_Data_Types.sql
@@ -179,3 +179,14 @@ SELECT *
 FROM `employees`;
 
 # 15.Basic Select All Fields and Order Them
+SELECT *
+FROM `towns`
+ORDER BY `name` ASC;
+
+SELECT *
+FROM `departments`
+ORDER BY `name` ASC;
+
+SELECT *
+FROM `employees`
+ORDER BY `salary` DESC;
