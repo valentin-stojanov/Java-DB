1f55ecb20dd2915e3480559abefad6c8eeb4c3d2
valentin <valentin_stojanov@abv.bg>
2022-01-16 21:46:49 +0200

    MySQL Lab Built-in Functions - added 2 and 3.

--- MySQL/Lab/Built-in-Functions/built-in_functions.sql
+++ MySQL/Lab/Built-in-Functions/built-in_functions.sql
@@ -4,3 +4,13 @@ FROM `books`
 -- WHERE substring(`title`, 1, 3) = 'The' 
 WHERE `title` LIKE 'The%'
 ORDER BY `id`;
+
+# 2.Replace Titles
+SELECT replace(`title`, 'The', '***') as 'Title'
+FROM `books`
+WHERE substring(`title`, 1, 3) = 'The'
+ORDER BY `id`;
+
+# 3.Sum Cost of All Books
+SELECT round(sum(`cost`), 2)
+FROM `books`;
