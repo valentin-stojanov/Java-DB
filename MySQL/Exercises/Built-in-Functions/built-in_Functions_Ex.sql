6974c85a3caaee7f78620c1c6ecaa123187126c8
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-17 16:24:04 +0200

    MySQL Exercises modified 01. Find Names of All Employees by First Name.

--- MySQL/Exercises/Built-in-Functions/built-in_Functions_Ex.sql
+++ MySQL/Exercises/Built-in-Functions/built-in_Functions_Ex.sql
@@ -2,5 +2,7 @@
 SELECT `first_name`, `last_name`
 FROM `employees`
 WHERE `first_name` LIKE 'Sa%'
+# WHERE left(`first_name`, 2) = 'Sa'
+# WHERE substring(`first_name`, 1, 2) = 'Sa'
 ORDER BY `employee_id`;
 
