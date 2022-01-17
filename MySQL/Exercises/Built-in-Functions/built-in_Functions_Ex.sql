1fc2074124677dd18333017995d55f9164730f74
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-17 16:57:45 +0200

    MySQL Exercises added 03.Find First Names of All Employees.

--- MySQL/Exercises/Built-in-Functions/built-in_Functions_Ex.sql
+++ MySQL/Exercises/Built-in-Functions/built-in_Functions_Ex.sql
@@ -6,8 +6,15 @@ WHERE `first_name` LIKE 'Sa%'
 # WHERE substring(`first_name`, 1, 2) = 'Sa'
 ORDER BY `employee_id`;
 
-# 02. Find Names of All Employees by Last Name
+# 02.Find Names of All Employees by Last Name
 SELECT `first_name`, `last_name`
 FROM `employees`
 WHERE `last_name` LIKE '%ei%'
+ORDER BY `employee_id`;
+
+# 03.Find First Names of All Employees.
+SELECT `first_name`
+FROM `employees`
+WHERE `department_id` IN (3, 10) 
+AND YEAR(`hire_date`) BETWEEN 1995 AND 2005
 ORDER BY `employee_id`;
