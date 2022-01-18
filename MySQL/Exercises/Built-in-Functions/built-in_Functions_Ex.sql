17fde0c7601315a554eecc4c21802572a715dd17
valentin <valentin_stojanov@abv.bg>
2022-01-18 22:14:14 +0200

    MySQL Exercises added 4.Find All Employees Except Engineers.

--- MySQL/Exercises/Built-in-Functions/built-in_Functions_Ex.sql
+++ MySQL/Exercises/Built-in-Functions/built-in_Functions_Ex.sql
@@ -18,3 +18,9 @@ FROM `employees`
 WHERE `department_id` IN (3, 10) 
 AND YEAR(`hire_date`) BETWEEN 1995 AND 2005
 ORDER BY `employee_id`;
+
+# 4.Find All Employees Except Engineers
+SELECT `first_name`, `last_name`
+FROM `employees`
+WHERE `job_title` NOT LIKE '%engineer%'
+ORDER BY `employee_id`;
