d738f24376319fa2f4aac7720af3be9b18062d56
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-19 15:19:23 +0200

    MySQL Exercises Built-in-Functions added 16.Orders Table.

--- MySQL/Exercises/Built-in-Functions/built-in_Functions_Ex.sql
+++ MySQL/Exercises/Built-in-Functions/built-in_Functions_Ex.sql
@@ -118,3 +118,11 @@ SELECT
         END
     ) AS 'Duration'
 FROM `games`;
+
+# 16.Orders Table
+SELECT 
+	`product_name`,
+    `order_date`,
+    date_add(`order_date`, interval 3 day) AS `pay_due`,
+    date_add(`order_date`, interval 1 month) AS `deliver_due`
+FROM `orders`;
