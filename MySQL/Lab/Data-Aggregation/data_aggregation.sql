cc24296c7f79d7e7c421501499d50e3faf35a00b
valentin <valentin_stojanov@abv.bg>
2022-01-20 20:41:13 +0200

    Lab Data-Aggregation added 3.Min Salary.

--- MySQL/Lab/Data-Aggregation/data_aggregation.sql
+++ MySQL/Lab/Data-Aggregation/data_aggregation.sql
@@ -11,7 +11,17 @@ ORDER BY `department_id`;
 # 2.Average Salary
 SELECT
 	`department_id`,
-    round(avg(`salary`), 2)
+    round(avg(`salary`), 2) AS `Average salary`
 FROM `employees`
 GROUP BY `department_id`
 ORDER BY `department_id`;
+
+# 3.Min Salary
+SELECT 
+	`department_id`,
+    round(min(`salary`), 2) AS `Min Salary`
+FROM `employees`
+GROUP BY `department_id`
+HAVING `Min Salary` > 800
+ORDER BY `department_id`;
+
