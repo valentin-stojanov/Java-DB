46794360e64b820b9f719770fff62c154ffc3c61
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-21 11:48:04 +0200

    MySQL Exercises - Data-Aggregation added ex.1 and 2.

--- MySQL/Exercises/Data-Aggregation/data_aggregation.sql
+++ MySQL/Exercises/Data-Aggregation/data_aggregation.sql
@@ -3,3 +3,16 @@ USE `gringotts`;
 # 1.Records' Count
 SELECT COUNT(*) AS `count`
 FROM `wizzard_deposits`;
+
+# 2.Longest Magic Wand
+-- SELECT `magic_wand_size` AS `longest_magic_wand`
+SELECT max(`magic_wand_size`) AS `longest_magic_wand`
+FROM `wizzard_deposits`;
+-- ORDER BY `magic_wand_size` DESC
+-- LIMIT 1;
+
+# 3.Longest Magic Wand Per Deposit Groups
+SELECT `deposit_group`, max(`magic_wand_size`) AS `longest_magic_wand`
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `longest_magic_wand`, `deposit_group`;
