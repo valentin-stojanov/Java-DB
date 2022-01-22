fb13e70dc3d29941b51d41e9adf1f66802b7212f
valentin <valentin_stojanov@abv.bg>
2022-01-22 23:53:08 +0200

    MySQL Exercises - Data-Aggregation added ex.7.Deposits Filter.

--- MySQL/Exercises/Data-Aggregation/data_aggregation.sql
+++ MySQL/Exercises/Data-Aggregation/data_aggregation.sql
@@ -30,3 +30,13 @@ FROM `wizzard_deposits`
 GROUP BY `deposit_group`
 ORDER BY `total_sum` ASC;
 
+# 6.Deposits Sum for Ollivander Family
+
+# 7.Deposits Filter
+SELECT  `deposit_group`, sum(`deposit_amount`) AS `total_sum`
+FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = 'Ollivander family'
+GROUP BY `deposit_group`
+HAVING `total_sum` < 150000
+ORDER BY `total_sum` DESC;
+
