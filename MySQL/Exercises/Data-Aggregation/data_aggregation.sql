b1c8574e09c568aa40879baef1b04e8987948158
valentin <valentin_stojanov@abv.bg>
2022-01-22 23:55:13 +0200

    MySQL Exercises - Data-Aggregation added ex.6.Deposits Sum for Ollivander Family.

--- MySQL/Exercises/Data-Aggregation/data_aggregation.sql
+++ MySQL/Exercises/Data-Aggregation/data_aggregation.sql
@@ -31,6 +31,11 @@ GROUP BY `deposit_group`
 ORDER BY `total_sum` ASC;
 
 # 6.Deposits Sum for Ollivander Family
+SELECT  `deposit_group`, sum(`deposit_amount`) AS `total_sum`
+FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = 'Ollivander family'
+GROUP BY `deposit_group`
+ORDER BY `deposit_group` ASC;
 
 # 7.Deposits Filter
 SELECT  `deposit_group`, sum(`deposit_amount`) AS `total_sum`
