3e0adb4236763cd47ac77f61c32ed30ba87aa47f
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-24 12:01:04 +0200

    MySQL Exercises - Data-Aggregation added ex. 10.First Letter.

--- MySQL/Exercises/Data-Aggregation/data_aggregation.sql
+++ MySQL/Exercises/Data-Aggregation/data_aggregation.sql
@@ -56,17 +56,28 @@ ORDER BY `magic_wand_creator`, `deposit_group`;
 
 # 9.Age Groups
 SELECT 
-	(CASE
-		WHEN `age` BETWEEN 0 AND 10 THEN '[0-10]'
-		WHEN `age` BETWEEN 11 AND 20 THEN '[11-20]'
-		WHEN `age` BETWEEN 21 AND 30 THEN '[21-30]'
-		WHEN `age` BETWEEN 31 AND 40 THEN '[31-40]'
-		WHEN `age` BETWEEN 41 AND 50 THEN '[41-50]'
-		WHEN `age` BETWEEN 51 AND 60 THEN '[51-60]'
+    (CASE
+        WHEN `age` BETWEEN 0 AND 10 THEN '[0-10]'
+        WHEN `age` BETWEEN 11 AND 20 THEN '[11-20]'
+        WHEN `age` BETWEEN 21 AND 30 THEN '[21-30]'
+        WHEN `age` BETWEEN 31 AND 40 THEN '[31-40]'
+        WHEN `age` BETWEEN 41 AND 50 THEN '[41-50]'
+        WHEN `age` BETWEEN 51 AND 60 THEN '[51-60]'
         ELSE '[61+]'
     END) AS `age_group`,
-    count(*) AS `wiz_count`
-FROM `wizzard_deposits`
+    COUNT(*) AS `wiz_count`
+FROM
+    `wizzard_deposits`
 GROUP BY `age_group`
 ORDER BY `age_group`;
+
+# 10.First Letter
+SELECT 
+    LEFT(`first_name`, 1) AS `first_letter`
+FROM
+    `wizzard_deposits`
+WHERE
+    `deposit_group` = 'Troll Chest'
+GROUP BY `first_letter`
+ORDER BY `first_letter`;
 
