ca64fb23e0cf003ba8b6c267675eb0abad010523
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-24 12:34:01 +0200

    MySQL Exercises - Data-Aggregation added ex. 12. Employees Minimum Salaries.

--- MySQL/Exercises/Data-Aggregation/data_aggregation.sql
+++ MySQL/Exercises/Data-Aggregation/data_aggregation.sql
@@ -83,10 +83,22 @@ ORDER BY `first_letter`;
 
 # 11. Average Interest
 SELECT 
-	`deposit_group`,
+    `deposit_group`,
     `is_deposit_expired`,
     AVG(`deposit_interest`) AS `average_interest`
-FROM `wizzard_deposits`
-WHERE `deposit_start_date` > '1985-01-01'
-GROUP BY `deposit_group`, `is_deposit_expired`
-ORDER BY `deposit_group` DESC, `is_deposit_expired` ASC;
+FROM
+    `wizzard_deposits`
+WHERE
+    `deposit_start_date` > '1985-01-01'
+GROUP BY `deposit_group` , `is_deposit_expired`
+ORDER BY `deposit_group` DESC , `is_deposit_expired` ASC;
+
+USE `soft_uni`;
+# 12. Employees Minimum Salaries
+SELECT 
+	`department_id`,
+    MIN(`salary`) AS `minimum_salary`
+FROM `employees`
+WHERE `department_id` IN(2, 5, 7) AND `hire_date` > '2000-01-01'
+GROUP BY `department_id` 
+ORDER BY `department_id`;
