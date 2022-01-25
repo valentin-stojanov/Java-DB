82a3df5a5ca991a45295e4c833212f51dba1367d
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-25 14:59:59 +0200

    MySQL Lab - Table Relations - added table_relations.sql and ex. 1. Mountains and Peaks.

---
+++ MySQL/Lab/Table-Relations/table_relations.sql
@@ -0,0 +1,16 @@
+USE `camp`;
+
+# 1. Mountains and Peaks
+CREATE TABLE `mountains`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE `peaks`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50),
+    `mountain_id` INT,
+    CONSTRAINT `fk_peaks_mountains`
+    FOREIGN KEY (`mountain_id`)
+    REFERENCES `mountains`(`id`)
+);
