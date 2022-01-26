af307a9afe55732897bd1b0cb860e90ff7d7a6db
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-26 11:38:52 +0200

    MySQL Lab - Table Relations - added 5. Project Management DB*.

--- MySQL/Lab/Table-Relations/table_relations.sql
+++ MySQL/Lab/Table-Relations/table_relations.sql
@@ -50,3 +50,36 @@ CREATE TABLE `peaks` (
         REFERENCES `mountains` (`id`)
         ON DELETE CASCADE
 );
+
+# 5. Project Management DB*
+CREATE DATABASE `project_managment`;
+USE `project_managment`;
+
+CREATE TABLE `clients`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `client_name` VARCHAR(100)
+);
+
+CREATE TABLE `projects`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `client_id` INT,
+    `project_lead_id` INT,
+    CONSTRAINT `fk_projects_clients`
+    FOREIGN KEY (`client_id`)
+    REFERENCES `clients`(`id`)
+);
+
+CREATE TABLE `employees`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `first_name` VARCHAR(30),
+    `last_name` VARCHAR(30),
+    `project_id` INT,
+    CONSTRAINT `fk_employees_projects`
+    FOREIGN KEY (`project_id`)
+    REFERENCES `projects`(`id`)
+);
+
+ALTER TABLE `projects`
+ADD CONSTRAINT `fk_project_lead_id_employees`
+FOREIGN KEY (`project_lead_id`)
+REFERENCES `employees`(`id`);
