a71191399ffa253d344f3aa8b265809ac55b7b23
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-26 15:34:55 +0200

    MySQL Exercises - Table Relations - added 3. Many-To-Many Relationship.

--- MySQL/Exercises/Table-Relations/table_relations_ex.sql
+++ MySQL/Exercises/Table-Relations/table_relations_ex.sql
@@ -58,4 +58,49 @@ VALUES
 ('Model S', 2),	
 ('Model X', 2),	
 ('Model 3', 2),	
-('Nova', 3);	
+('Nova', 3);
+
+# 3. Many-To-Many Relationship
+CREATE TABLE `students`(
+	`student_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(30)    
+);
+
+INSERT INTO `students`(`name`)
+VALUES
+('Mila'),
+('Toni'),
+('Ron');
+
+CREATE TABLE `exams`(
+	`exam_id` INT PRIMARY KEY AUTO_INCREMENT,
+	`name` VARCHAR(30)
+) AUTO_INCREMENT=101;
+
+INSERT INTO	`exams`(`name`)
+VALUES
+('Spring MVC'), 
+('Neo4j'), 
+('Oracle 11g'); 
+
+CREATE TABLE `students_exams`(
+	`student_id` INT NOT NULL,
+    `exam_id` INT NOT NULL,
+    CONSTRAINT pk_student_exam
+    PRIMARY KEY (`student_id`, `exam_id`),
+    CONSTRAINT fk_students_exam_student
+    FOREIGN KEY (`student_id`)
+    REFERENCES `students`(`student_id`),
+	CONSTRAINT fk_students_exams_exams
+	FOREIGN KEY (`exam_id`)
+	REFERENCES `exams`(`exam_id`)
+);
+
+INSERT INTO `students_exams`
+VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103);
