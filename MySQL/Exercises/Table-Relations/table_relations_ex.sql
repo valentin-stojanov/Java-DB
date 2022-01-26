e93dd0cedb84f4067e1fc0bbb52e6577df7744e9
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-26 13:55:31 +0200

    MySQL Exercises - Table Relations - added 02. One-To-Many Relationship.

--- MySQL/Exercises/Table-Relations/table_relations_ex.sql
+++ MySQL/Exercises/Table-Relations/table_relations_ex.sql
@@ -28,3 +28,34 @@ VALUES
 ('Roberto', 43300.00, 102),
 ('Tom', 56100.00, 103),
 ('Yana', 60200.00, 101);
+
+# 02. One-To-Many Relationship
+CREATE TABLE `manufacturers`(
+	`manufacturer_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20),
+    `established_on` DATE
+);
+
+INSERT INTO `manufacturers`(`name`, `established_on`)
+VALUES
+('BMW', '1916-03-01'),
+('Tesla', '2003-01-01'),
+('Lada', '1966-05-01');
+
+CREATE TABLE `models`(
+	`model_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20),
+    `manufacturer_id` INT,
+    CONSTRAINT `fk_models_manufacturer`
+    FOREIGN KEY (`manufacturer_id`)
+    REFERENCES `manufacturers`(`manufacturer_id`)
+)AUTO_INCREMENT = 101 ;
+
+INSERT INTO `models`(`name`, `manufacturer_id`)
+VALUES
+('X1', 1),	
+('i6', 1),	
+('Model S', 2),	
+('Model X', 2),	
+('Model 3', 2),	
+('Nova', 3);	
