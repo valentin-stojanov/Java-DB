b69f72601d6d240d85c14a2570fcfdbd51603445
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-26 11:02:53 +0200

    MySQL Lab - Table Relations - added 4. Delete Mountains.

--- MySQL/Lab/Table-Relations/table_relations.sql
+++ MySQL/Lab/Table-Relations/table_relations.sql
@@ -35,3 +35,18 @@ FROM
     `routes` AS r
         JOIN
     `campers` AS c ON r.`leader_id` = c.`id`;
+    
+# 4. Delete Mountains
+CREATE TABLE `mountains` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `peaks` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `mountain_id` INT,
+    CONSTRAINT `fk_peaks_mountains` FOREIGN KEY (`mountain_id`)
+        REFERENCES `mountains` (`id`)
+        ON DELETE CASCADE
+);
