617bc1e310f95f681c111514dfc0034ff631d091
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-26 10:35:52 +0200

    MySQL Lab - Table Relations - added 3. SoftUni Hiking.

--- MySQL/Lab/Table-Relations/table_relations.sql
+++ MySQL/Lab/Table-Relations/table_relations.sql
@@ -25,3 +25,13 @@ FROM
         JOIN
     `vehicles` AS v ON v.`driver_id` = c.`id`;
 
+# 3. SoftUni Hiking
+SELECT 
+    `starting_point`,
+    `end_point`,
+    `leader_id`,
+    CONCAT(`first_name`, ' ', `last_name`) AS `leader_name`
+FROM
+    `routes` AS r
+        JOIN
+    `campers` AS c ON r.`leader_id` = c.`id`;
