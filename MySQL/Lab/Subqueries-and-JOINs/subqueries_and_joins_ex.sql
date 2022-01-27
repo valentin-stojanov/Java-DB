84ddf01aada558c60cc42f894804f8041d971d4d
valentin <valentin_stojanov@abv.bg>
2022-01-27 23:42:33 +0200

    MySQL Lab - Subqueries and JOINs - added subqueries_and_joins_ex.sql and ex. 1. Managers.

---
+++ MySQL/Lab/Subqueries-and-JOINs/subqueries_and_joins_ex.sql
@@ -0,0 +1,8 @@
+
+# 1. Managers
+SELECT employee_id, concat(`first_name`, ' ', `last_name`) AS ' full_name', d.department_id, d.name
+FROM employees AS e
+JOIN departments AS d
+ON e.employee_id = d.manager_id
+ORDER BY employee_id
+LIMIT 5;
