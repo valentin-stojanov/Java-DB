a515b59a46ee700a4d1f78e37413a972a201c90b
valentin-stojanov <valentin_stojanov@abv.bg>
2022-01-31 12:18:24 +0200

    MySQL Lab - Subqueries and JOINs - added ex. 2. Towns Addresses.

--- MySQL/Lab/Subqueries-and-JOINs/subqueries_and_joins_ex.sql
+++ MySQL/Lab/Subqueries-and-JOINs/subqueries_and_joins_ex.sql
@@ -6,3 +6,11 @@ JOIN departments AS d
 ON e.employee_id = d.manager_id
 ORDER BY employee_id
 LIMIT 5;
+
+# 2. Towns Addresses
+SELECT t.`town_id`, t.`name`, a.`address_text`
+FROM `addresses` AS a
+JOIN `towns` AS t
+ON t.`town_id` = a.`town_id`
+WHERE t.`name` IN('San Francisco', 'Sofia', 'Carnation')
+ORDER BY t.`town_id`, a.`address_id`;
