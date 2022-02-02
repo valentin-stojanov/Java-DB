8bac50c003ea6a12b2c5141876b4592c076f5574
valentin-stojanov <valentin_stojanov@abv.bg>
2022-02-02 14:33:15 +0200

    MySQL Exercises - Subqueries and JOINs - added ex. 04. Employee Departments.

--- MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
+++ MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
@@ -39,3 +39,17 @@ FROM
 WHERE
     d.`name` = 'Sales'
 ORDER BY e.`employee_id` DESC;
+
+# 04. Employee Departments
+SELECT 
+    e.`employee_id`,
+    e.`first_name`,
+    e.`salary`, 
+    d.`name`
+FROM
+    `employees` AS e
+        JOIN
+    `departments` AS d ON e.`department_id` = d.`department_id`
+WHERE e.`salary` > 15000
+ORDER BY d.`department_id` DESC
+LIMIT 5;
