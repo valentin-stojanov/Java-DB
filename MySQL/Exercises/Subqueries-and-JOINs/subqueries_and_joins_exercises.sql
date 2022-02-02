b497722cf4e88b6cf57d4cc8ab7cf176861f2552
valentin-stojanov <valentin_stojanov@abv.bg>
2022-02-02 14:20:41 +0200

    MySQL Exercises - Subqueries and JOINs - added ex. 02. Addresses with Towns.

--- MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
+++ MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
@@ -10,3 +10,18 @@ FROM
     `addresses` AS a ON e.`address_id` = a.`address_id`
 ORDER BY a.`address_id` ASC
 LIMIT 5;
+
+# 02. Addresses with Towns
+SELECT 
+    e.`first_name`,
+    e.`last_name`,
+    t.`name`,
+    a.`address_text`
+FROM
+    `employees` AS e
+        JOIN
+    `addresses` AS a ON e.`address_id` = a.`address_id`
+        JOIN
+    `towns` AS t ON a.`town_id` = t.`town_id`
+ORDER BY e.`first_name` , e.`last_name`
+LIMIT 5;
