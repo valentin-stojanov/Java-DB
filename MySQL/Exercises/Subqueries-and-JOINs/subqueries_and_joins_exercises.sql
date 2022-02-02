5ca8f7bfd490fb49796fcbe5aad98a17fa1ced95
valentin-stojanov <valentin_stojanov@abv.bg>
2022-02-02 16:10:06 +0200

    MySQL Exercises - Subqueries and JOINs - added ex. 10. Employee Summary.

--- MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
+++ MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
@@ -124,3 +124,18 @@ FROM
 WHERE
     e.`manager_id` IN (3 , 7)
 ORDER BY e.`first_name`;
+
+# 10. Employee Summary
+SELECT 
+    e.`employee_id`,
+    CONCAT_WS(' ', e.`first_name`, e.`last_name`) AS 'employee_name',
+    CONCAT_WS(' ', m.`first_name`, m.`last_name`) AS 'manager_name',
+    d.`name`
+FROM
+    `employees` AS e
+        JOIN
+    `employees` AS m ON e.`manager_id` = m.`employee_id`
+        JOIN
+    `departments` AS d ON e.`department_id` = d.`department_id`
+ORDER BY e.`employee_id`
+LIMIT 5;
