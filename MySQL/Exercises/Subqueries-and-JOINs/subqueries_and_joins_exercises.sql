a2538133ce9add944d9f8a3ccb0a7e4a2ad7470f
valentin-stojanov <valentin_stojanov@abv.bg>
2022-02-02 15:41:57 +0200

    MySQL Exercises - Subqueries and JOINs - added ex. 09. Employee Manager.

--- MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
+++ MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
@@ -110,3 +110,17 @@ FROM
 WHERE
     e.`employee_id` = 24
 ORDER BY p.`name`;
+
+# 09. Employee Manager
+SELECT 
+    e.`employee_id`,
+    e.`first_name`,
+    m.`employee_id` AS 'manager_id',
+    m.`first_name` AS 'manager_name'
+FROM
+    `employees` AS e
+        JOIN
+    `employees` AS m ON e.`manager_id` = m.`employee_id`
+WHERE
+    e.`manager_id` IN (3 , 7)
+ORDER BY e.`first_name`;
