b45fc9d9e1da2f997eae31ba8e6a10f948674334
valentin-stojanov <valentin_stojanov@abv.bg>
2022-02-02 15:16:14 +0200

    MySQL Exercises - Subqueries and JOINs - added ex. 07. Employees with Project.

--- MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
+++ MySQL/Exercises/Subqueries-and-JOINs/subqueries_and_joins_exercises.sql
@@ -78,3 +78,18 @@ WHERE
     DATE(e.`hire_date`) > '1991-01-01'
         AND d.`name` IN ('Sales' , 'Finance')
 ORDER BY e.`hire_date` ASC;
+
+# 07. Employees with Project
+SELECT 
+    e.`employee_id`, e.`first_name`, p.`name`
+FROM
+    `employees` AS e
+        JOIN
+    `employees_projects` AS ep ON e.`employee_id` = ep.`employee_id`
+        JOIN
+    `projects` AS p ON ep.`project_id` = p.`project_id`
+WHERE
+    DATE(p.`start_date`) > '2002-08-13'
+        AND DATE(p.`end_date`) IS NULL
+ORDER BY e.`first_name` , p.`name`
+LIMIT 5;
