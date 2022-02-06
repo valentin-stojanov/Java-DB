7f45953ac640fa3f3d2fd3adfd08599796640eec
valentin-stojanov <valentin_stojanov@abv.bg>
2022-02-06 15:53:40 +0200

    MySQL Excercises Functions and Procedures - added functions_and_procedures_exercises.sql and ex. 1. Employees with Salary Above 35000.

---
+++ MySQL/Exercises/Functions and Procedures/functions_and_procedures_exercises.sql
@@ -0,0 +1,13 @@
+# 1. Employees with Salary Above 35000
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_salary_above_35000()
+BEGIN
+	SELECT `first_name`, `last_name`
+	FROM `employees`
+	WHERE `salary` > 35000
+	ORDER BY `first_name`, `last_name`;
+END$$
+DELIMITER ;
+
+CALL usp_get_employees_salary_above_35000();
+
