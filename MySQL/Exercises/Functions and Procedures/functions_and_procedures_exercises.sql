6363ac7077c649fc7c02e87a470e03ded02eeb4f
valentin <valentin_stojanov@abv.bg>
2022-02-07 21:29:19 +0200

    MySQL Excercises Functions and Procedures - added ex. 5. Salary Level Function.

--- MySQL/Exercises/Functions and Procedures/functions_and_procedures_exercises.sql
+++ MySQL/Exercises/Functions and Procedures/functions_and_procedures_exercises.sql
@@ -54,4 +54,24 @@ DELIMITER ;
 
 CALL usp_get_employees_from_town('Sofia');
 
-# 
+# 5. Salary Level Function
+DELIMITER $$
+CREATE FUNCTION ufn_get_salary_level(salary DOUBLE)
+RETURNS VARCHAR(7)
+DETERMINISTIC
+	BEGIN
+		DECLARE salary_level VARCHAR(7);
+        SET salary_level := (
+			CASE 
+				WHEN salary < 30000 THEN 'Low'
+				WHEN salary <= 50000 THEN 'Average'
+                ELSE 'High'
+			END
+        );
+        RETURN salary_level;
+    END$$
+    DELIMITER ;
+    
+    SELECT ufn_get_salary_level(50000); -- TEST INPUT
+    DROP FUNCTION ufn_get_salary_level;
+	
