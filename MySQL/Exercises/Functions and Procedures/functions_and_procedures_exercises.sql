b8543fb320cff496066ab54f612124f7b32c15b4
valentin <valentin_stojanov@abv.bg>
2022-02-07 20:28:07 +0200

    MySQL Excercises Functions and Procedures - added ex. 2. Employees with Salary Above Number.

--- MySQL/Exercises/Functions and Procedures/functions_and_procedures_exercises.sql
+++ MySQL/Exercises/Functions and Procedures/functions_and_procedures_exercises.sql
@@ -11,3 +11,15 @@ DELIMITER ;
 
 CALL usp_get_employees_salary_above_35000();
 
+# 2. Employees with Salary Above Number
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_salary_above(num DECIMAL(19,4))
+BEGIN
+	SELECT `first_name`, `last_name`
+	FROM `employees`
+	WHERE `salary` >= num
+    ORDER BY `first_name`, `last_name`, `employee_id`;
+END$$
+DELIMITER ;
+
+CALL usp_get_employees_salary_above(45000);
