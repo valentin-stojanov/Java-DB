8a66cc9c480afb97b9771276e96c5246519f43be
valentin-stojanov <valentin_stojanov@abv.bg>
2022-02-08 14:32:54 +0200

    MySQL Excercises Functions and Procedures - added ex. 11. Calculating Interest.

--- MySQL/Exercises/Functions and Procedures/functions_and_procedures_exercises.sql
+++ MySQL/Exercises/Functions and Procedures/functions_and_procedures_exercises.sql
@@ -181,3 +181,22 @@ DELIMITER ;
 SELECT ufn_calculate_future_value(1000.55, 0.08, 5);
 DROP FUNCTION ufn_calculate_future_value;
 
+# 11. Calculating Interest
+DELIMITER $$
+CREATE PROCEDURE usp_calculate_future_value_for_account(ac_id INT, interest_rate DECIMAL(19,4))
+BEGIN
+	SELECT 
+		a.`id`,
+		ah.`first_name`, 
+		ah.`last_name`, 
+		a.`balance` AS `current_balance`,
+		ufn_calculate_future_value(a.`balance`, interest_rate, 5) AS `balance_in5_year`    
+	FROM `accounts` AS a
+	JOIN `account_holders` AS ah
+	ON a.`account_holder_id` = ah.`id`
+	WHERE a.`id` = ac_id;
+END$$
+DELIMITER ;
+
+CALL usp_calculate_future_value_for_account(1, 0.1);
+DROP PROCEDURE usp_calculate_future_value_for_account;
