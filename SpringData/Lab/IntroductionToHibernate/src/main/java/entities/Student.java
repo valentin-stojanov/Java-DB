5eb08d709c39873f7e4979143af9942a6f8c6ba5
valentin <valentin_stojanov@abv.bg>
2022-02-23 23:36:52 +0200

    Java-DB\SpringData\Lab\IntroductionToHibernate - added Main and Student.java to IntroductionToHibernate.

---
+++ SpringData/Lab/IntroductionToHibernate/src/main/java/entities/Student.java
@@ -0,0 +1,50 @@
+package entities;
+
+import com.sun.xml.bind.v2.runtime.output.C14nXmlOutput;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    public Student() {
+
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
