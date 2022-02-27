a1f1bb5bc6f0c615833adb250ef507f1430678f4
valentin <valentin_stojanov@abv.bg>
2022-02-27 23:48:23 +0200

    Java-DB\SpringData\Exercises\introductionToHibernate - some changes.

--- SpringData/Exercises/introductionToHibernate/introtohibernate_ex_skeleton/src/main/java/entities/Town.java
+++ SpringData/Exercises/introductionToHibernate/introtohibernate_ex_skeleton/src/main/java/entities/Town.java
@@ -27,4 +27,12 @@ public class Town {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Town{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
