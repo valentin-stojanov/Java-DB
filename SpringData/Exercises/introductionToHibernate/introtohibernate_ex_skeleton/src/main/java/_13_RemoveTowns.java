82d1e852ac8d90333344382af6896297567ded50
valentin-stojanov <valentin_stojanov@abv.bg>
2022-03-04 12:26:11 +0200

    Java-DB\SpringData\Exercises\introductionToHibernate - added _13_RemoveTowns.java

--- SpringData/Exercises/introductionToHibernate/introtohibernate_ex_skeleton/src/main/java/_13_RemoveTowns.java
+++ SpringData/Exercises/introductionToHibernate/introtohibernate_ex_skeleton/src/main/java/_13_RemoveTowns.java
@@ -1,6 +1,10 @@
+import entities.Address;
+import entities.Town;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 import java.util.Scanner;
 
 public class _13_RemoveTowns {
@@ -8,24 +12,37 @@ public class _13_RemoveTowns {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Name");
         EntityManager entityManager = factory.createEntityManager();
 
-        entityManager.getTransaction().begin();
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter town name:");
         String townName = scanner.nextLine();
 
-        Integer addressId = entityManager.createQuery("select a.id from Address a where a.town.name = :townName", Integer.class)
+        Town town = entityManager
+                .createQuery("select t from Town t where t.name = :townName",
+                        Town.class)
                 .setParameter("townName", townName)
                 .getSingleResult();
 
-        entityManager.createQuery("delete from Town a where a.name = :townName")
-                .setParameter("townName", townName)
-                .executeUpdate();
+        int affectedRows = removeAddressesById(town.getId(), entityManager);
 
-        int deletedAddresses = entityManager.createQuery("DELETE from Address a where a.id = :addressId")
-                .setParameter("addressId", addressId)
-                .executeUpdate();
-
-        System.out.println(deletedAddresses);
+        entityManager.getTransaction().begin();
+        entityManager.remove(town);
         entityManager.getTransaction().commit();
+
+        System.out.printf("%d addresses in %s is deleted", affectedRows, townName);
+
+    }
+
+    private static int removeAddressesById(Integer id, EntityManager entityManager) {
+        List<Address> addresses = entityManager
+                .createQuery("SELECT a FROM Address a WHERE a.town.id = :townId", Address.class)
+                .setParameter("townId", id)
+                .getResultList();
+
+        entityManager.getTransaction().begin();
+        addresses.forEach(entityManager::remove);
+        entityManager.getTransaction().commit();
+
+        return addresses.size();
     }
 }
