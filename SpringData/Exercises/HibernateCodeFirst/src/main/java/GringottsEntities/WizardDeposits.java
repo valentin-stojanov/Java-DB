eb17ce941e8d14fc15a2180c566adaf3d4117e0d
valentin <valentin_stojanov@abv.bg>
2022-03-06 20:30:48 +0200

    Java-DB\SpringData\Exercises\HibernateCodeFirst - added ex. 1.Gringotts Database.

---
+++ SpringData/Exercises/HibernateCodeFirst/src/main/java/GringottsEntities/WizardDeposits.java
@@ -0,0 +1,156 @@
+package GringottsEntities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wizard_deposits")
+public class WizardDeposits {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String notes;
+    private Integer age;
+    private String magicWandCreator;
+    private Integer magicWandSize;
+    private String depositGroup;
+    private LocalDateTime depositStartDate;
+    private BigDecimal depositAmount;
+    private Double depositInterest;
+    private Double depositCharge;
+    private LocalDateTime depositExpirationDate;
+    private Boolean isDepositExpired;
+
+    public WizardDeposits() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "first_name", length = 50)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", length = 60, nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "notes", columnDefinition = "TEXT")
+//    @Column(name = "notes", length = 1000)
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Column(name = "age", nullable = false)
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Column(name = "magic_wand_creator", length = 100)
+    public String getMagicWandCreator() {
+        return magicWandCreator;
+    }
+
+    public void setMagicWandCreator(String magicWandCreator) {
+        this.magicWandCreator = magicWandCreator;
+    }
+
+    @Column(name = "magic_wand-size", nullable = false)
+    public Integer getMagicWandSize() {
+        return magicWandSize;
+    }
+
+    public void setMagicWandSize(Integer magicWandSize) {
+        this.magicWandSize = magicWandSize;
+    }
+
+    @Column(name = "deposit_group", length = 20)
+    public String getDepositGroup() {
+        return depositGroup;
+    }
+
+    public void setDepositGroup(String depositGroup) {
+        this.depositGroup = depositGroup;
+    }
+
+    @Column(name = "deposit_start_date")
+    public LocalDateTime getDepositStartDate() {
+        return depositStartDate;
+    }
+
+    public void setDepositStartDate(LocalDateTime depositStartDate) {
+        this.depositStartDate = depositStartDate;
+    }
+
+    @Column(name = "deposit_amount")
+    public BigDecimal getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(BigDecimal depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+    @Column(name = "deposit_interest")
+    public Double getDepositInterest() {
+        return depositInterest;
+    }
+
+    public void setDepositInterest(Double depositInterest) {
+        this.depositInterest = depositInterest;
+    }
+
+    @Column(name = "deposit_charge")
+    public Double getDepositCharge() {
+        return depositCharge;
+    }
+
+    public void setDepositCharge(Double depositCharge) {
+        this.depositCharge = depositCharge;
+    }
+
+    @Column(name = "deposit_expiration_date")
+    public LocalDateTime getDepositExpirationDate() {
+        return depositExpirationDate;
+    }
+
+    public void setDepositExpirationDate(LocalDateTime depositExpirationDate) {
+        this.depositExpirationDate = depositExpirationDate;
+    }
+
+    @Column(name = "is_deposit_expired")
+    public Boolean getDepositExpired() {
+        return isDepositExpired;
+    }
+
+    public void setDepositExpired(Boolean depositExpired) {
+        isDepositExpired = depositExpired;
+    }
+}
---
+++ SpringData/Exercises/HibernateCodeFirst/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManager entityManager = Persistence.createEntityManagerFactory("test").createEntityManager();
+
+
+
+    }
+
+}
