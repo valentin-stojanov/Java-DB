4c65463773890a41f766820ba1414d2338814ae0
valentin-stojanov <valentin_stojanov@abv.bg>
2022-03-31 11:52:37 +0300

    Java-DB\SpringData\Exercises\Exam_preparation\Real_Deal implemented CarService (without getCarsOrderByPicturesCountThenByMake() ).

--- SpringData/Exercises/Exam_preparation/Real_Deal/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ SpringData/Exercises/Exam_preparation/Real_Deal/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -2,10 +2,16 @@ package softuni.exam.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import softuni.exam.util.ValidationUtil;
+import softuni.exam.util.impl.ValidationUtilImpl;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
 @Configuration
@@ -19,12 +25,19 @@ public class ApplicationBeanConfiguration {
 
     @Bean
     public ValidationUtil validationUtil() {
-        return null;
+        return new ValidationUtilImpl();
     }
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.addConverter(new Converter<String, LocalDate>() {
+            @Override
+            public LocalDate convert(MappingContext<String, LocalDate> mappingContext) {
+                return LocalDate.parse(mappingContext.getSource(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            }
+        });
+        return modelMapper;
     }
 
 }
--- SpringData/Exercises/Exam_preparation/Real_Deal/src/main/java/softuni/exam/service/impl/CarServiceImpl.java
+++ SpringData/Exercises/Exam_preparation/Real_Deal/src/main/java/softuni/exam/service/impl/CarServiceImpl.java
@@ -1,22 +1,41 @@
 package softuni.exam.service.impl;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import softuni.exam.models.dto.CarImportDTO;
+import softuni.exam.models.entity.Car;
 import softuni.exam.repository.CarRepository;
 import softuni.exam.service.CarService;
+import softuni.exam.util.ValidationUtil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService {
     public static final Path CAR_FILE_PATH = Path.of("src/main/resources/files/json/cars.json");
     private final CarRepository carRepository;
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+    private final ValidationUtil validationUtil;
 
-    public CarServiceImpl(CarRepository carRepository) {
+    @Autowired
+    public CarServiceImpl(CarRepository carRepository,
+                          ModelMapper modelMapper,
+                          Gson gson,
+                          ValidationUtil validationUtil) {
         this.carRepository = carRepository;
+        this.modelMapper = modelMapper;
+        this.gson = gson;
+        this.validationUtil = validationUtil;
+
     }
 
     @Override
@@ -34,7 +53,20 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public String importCars() throws IOException {
-        return null;
+        CarImportDTO[] carImportDTO = this.gson.fromJson(readCarsFileContent(), CarImportDTO[].class);
+
+        StringBuilder result = new StringBuilder();
+        for (CarImportDTO carDTO : carImportDTO) {
+            boolean isValidDTO = this.validationUtil.isValid(carDTO);
+            if (!isValidDTO){
+                result.append("Invalid car\n");
+                continue;
+            }
+            Car car = this.modelMapper.map(carDTO, Car.class);
+            this.carRepository.save(car);
+            result.append(String.format("Successfully imported car - %s - %s\n",carDTO.getMake(), carDTO.getModel()));
+        }
+        return result.toString();
     }
 
     @Override
